e630c80f664aaad50fa623660364465d1881b19e
higor <higor1308@gmail.com>
2018-08-21 17:46:15 -0300

    Exercicio 2 finalizado

--- exercicio2.py
+++ exercicio2.py
@@ -3,9 +3,8 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-P = {'xm1': 2, 'sig1': 1, 'xm2': -2, 'sig2': 1, 'p1': 2, 
-			'q1': 0, 'p2': -2, 'q2': 0}
-D = [0,0,0,0,0,0,0,0]
+P = {'p1': 2.0, 'q1': 0, 'p2': -2.0, 'q2': 0, 'xm1': 2.0, 'sig1': 1.0, 'xm2': -2.0, 'sig2': 1.0}
+D = {'p1': 0, 'q1': 0, 'p2': 0, 'q2': 0, 'xm1': 0, 'sig1': 0, 'xm2': 0, 'sig2': 0}
 
 def yd(x):
 	return x**2
@@ -20,26 +19,28 @@ def y(x):
 	return ((w1(x)*y1(x))+(w2(x)*y2(x)))/(w1(x)+w2(x))
 
 def w1(x):
-	return math.exp((-1/2)*(((x-P['xm1'])/P['sig1'])**2))
+	return math.exp((-1/2.0)*(((x-P['xm1'])/P['sig1'])**2))
 
 def w2(x):
-	return math.exp((-1/2)*(((x-P['xm2'])/P['sig2'])**2))
+	return math.exp((-1/2.0)*(((x-P['xm2'])/P['sig2'])**2))
 
 def error(x):
-	return (1/2)*math.pow((y(x)-yd(x)), 2)
+	return (1/2.0)*math.pow((y(x)-yd(x)), 2)
 
 def derivative(x):
-	D[0] = (y(x)-yd(x))*(w1(x)/(w1(x)+w2(x)))*x
-	D[1] = (y(x)-yd(x))*(w2(x)/(w1(x)+w2(x)))*x
-	D[2] = (y(x)-yd(x))*(w1(x)/(w1(x)+w2(x)))
-	D[3] = (y(x)-yd(x))*(w2(x)/(w1(x)+w2(x)))
-	D[4] = (y(x)-yd(x))*w2(x)*(y1(x)-y2(x))/((w1(x)+w2(x))**2)*w1(x)*(x-P['xm1']/(P['sig1']**2))
-	D[5] = (y(x)-yd(x))*w1(x)*(y2(x)-y1(x))/((w1(x)+w2(x))**2)*w2(x)*(x-P['xm2']/(P['sig2']**2))
-	D[6] = (y(x)-yd(x))*w2(x)*(y1(x)-y2(x))/((w1(x)+w2(x))**2)*w1(x)*((x-P['xm1'])**2/(P['sig1']**3))
-	D[7] = (y(x)-yd(x))*w1(x)*(y2(x)-y1(x))/((w1(x)+w2(x))**2)*w2(x)*((x-P['xm2'])**2/(P['sig2']**3))
+	diffy = y(x) - yd(x)
+
+	D['p1'] = diffy*(w1(x)/float((w1(x)+w2(x))))*x
+	D['p2'] = diffy*(w2(x)/float((w1(x)+w2(x))))*x
+	D['q1'] = diffy*(w1(x)/float((w1(x)+w2(x))))
+	D['q2'] = diffy*(w2(x)/float((w1(x)+w2(x))))
+	D['xm1'] = diffy*w2(x)*(y1(x)-y2(x))/float((w1(x)+w2(x))**2)*w1(x)*(x-P['xm1']/float(P['sig1']**2))
+	D['xm2'] = diffy*w1(x)*(y2(x)-y1(x))/float((w1(x)+w2(x))**2)*w2(x)*(x-P['xm2']/float(P['sig2']**2))
+	D['sig1'] = diffy*w2(x)*(y1(x)-y2(x))/float((w1(x)+w2(x))**2)*w1(x)*((x-P['xm1'])**2/float(P['sig1']**3))
+	D['sig2'] = diffy*w1(x)*(y2(x)-y1(x))/float((w1(x)+w2(x))**2)*w2(x)*((x-P['xm2'])**2/float(P['sig2']**3))
 
 Xin = np.arange(-2, 2.04, 0.04)
-time = 50
+time = 500
 alpha = 0.01
 E = []
 for t in range(0, time):
@@ -47,21 +48,36 @@ for t in range(0, time):
 	e = 0
 	for i in index:
 		e += error(Xin[i])
-		print(e)
 		derivative(Xin[i])
-		j = 0
 		for key in P:
-			P[key] -= alpha*D[j]
-			j += 1
+			P[key] -= alpha*D[key]
 	E.append(e)
 
+
+plt.figure(1)
+
 plt.plot(E)
-plt.show()	
-"""
+plt.xlabel("Time");
+plt.ylabel("Error");
+
+Yd = []
+Y = []
+
+for x in Xin:
+	Y.append(y(x))
+	Yd.append(yd(x))
+
+plt.figure(2)
+
+plt.subplot(211)
 plt.xlabel("x");
 plt.ylabel("f(x)");
-plt.plot(x,Y, label="Expected output - f(x) = x²-0.2x-0.1");
-plt.plot(x,Z, label="Adjusted output");
+plt.plot(Xin,Yd, label="Expected output - f(x) = x²");
 plt.legend()
-plt.show();
-"""
+
+plt.subplot(212)
+plt.xlabel("x");
+plt.ylabel("f(x)");
+plt.plot(Xin,Y, label="Adjusted output");
+plt.legend()
+plt.show()
